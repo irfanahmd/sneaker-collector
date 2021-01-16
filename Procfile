fd2f9e76fe86116a7d693c75c8894cdd9bf078de
Irfan <irfan@NC-MBP.local>
2021-01-16 16:54:14 -0500

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sneakercollecter.wsgi
--- sneakercollector/settings.py
+++ sneakercollector/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +124,9 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/sneakers/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+# HEROKU CONFIG
+
+django_heroku.settings(locals())
+
+WSGI_APPLICATION = 'sneakercollector.wsgi.application'
